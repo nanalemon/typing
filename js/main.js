7eff3dc387c2c34429a72b7e308c94f111d237fa
nanalemon <86542818+nanalemon@users.noreply.github.com>
2021-11-07 23:16:29 +0900

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,104 @@
+'use strict';
+{
+  const wrap=document.getElementById('wrap');
+  const start=document.getElementById('start');
+
+  const textLists=['Hello World','This is my App','How are you?','Today is sunny','I love JavaScript!','Good morning',
+  'I am Japanese','Let it be','Samurai',
+  'Typing Game','Information Technology',
+  'I want to be a programmer','What day is today?',
+  'I want to build a web app','Nice to meet you',
+  'Chrome Firefox Edge Safari','machine learning',
+  'Brendan Eich','John Resig','React Vue Angular',
+  'Netscape Communications','undefined null NaN',
+  'Thank you very much','Google Apple Facebook Amazon',
+  'ECMAScript','console.log','for while if switch',
+  'var let const','Windows Mac Linux iOS Android',
+  'programming'];
+
+  let checkTexts=[];
+  let score=0;
+
+  const createText=()=>{
+    const p =document.getElementById('text');
+
+    const rnd=Math.floor(Math.random() * textLists.length);
+
+    p.textContent='';
+    
+    checkTexts=textLists[rnd].split('').map(value=>{
+
+      const span=document.createElement('span');
+      span.textContent=value;
+      p.appendChild(span);
+
+      return span;
+    })
+  };
+
+
+
+  const keyDown = e=>{
+    wrap.style.backgroundColor='#666'
+    if(e.key === checkTexts[0].textContent){
+      checkTexts[0].className='add-color';
+      checkTexts.shift();
+
+      score++;
+
+      if(!checkTexts.length) createText();
+    }else if(e.key==='shift'){
+      wrap.style.backgroundColor='#666';
+    }else{
+      wrap.style.backgroundColor='';
+    }
+  };
+
+  const rankCheck=score=>{
+    let text='';
+
+    if(score <100){
+      text=`あなたのランクはCです。Bランクまであと${100 - score}文字です。`;
+    }else if(score <200){
+      text=`あなたのランクはBです。Aランクまであと${200-score}文字です。`;
+    }else if(score <300){
+      text=`あなたのランクはAです。Sランクまであと${300 - score}文字です。`
+    }else if(score >=300){
+      text=`あなたのランクはSです。おめでとうございます！`;
+    }
+    return `${score}文字打てました！${text}【OK】リトライ【キャンセル】終了`;
+  };
+
+  const gameOver=id=>{
+    clearInterval(id);
+    
+    const result=confirm(rankCheck(score));
+
+    if(result)window.location.reload();
+  };
+
+  
+
+  const timer=()=>{
+    let time=60;
+
+
+    const count=document.getElementById('count');
+    const id=setInterval(()=>{
+      if(time<=0) gameOver(id);
+
+      count.textContent=time--;
+
+    },1000);
+
+  };
+
+  start.addEventListener('click',()=>{
+    timer();
+    createText();
+
+    start.style.display='none';
+
+    document.addEventListener('keydown',keyDown);
+  })
+}
